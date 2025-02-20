c63a8fa6d5bad697eb00ebbd21f56ba8f63121d9
Omar <codka1@hotmail.com>
2025-02-20 20:41:18 +0300

    property list main form added

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,10 +1,13 @@
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { Routes, RouterModule} from '@angular/router'
-import { FormsModule, ReactiveFormsModule} from '@angular/forms'
-
+import { Routes, RouterModule} from '@angular/router';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { AppComponent } from './app.component';
 import { PropertCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component'
@@ -46,7 +49,12 @@ const appRoutes:Routes = [
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
+    ButtonsModule.forRoot(),
+    BsDatepickerModule.forRoot()
   ],
   providers: [
     HousingService,
--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-add-property',
@@ -9,6 +10,13 @@ import { Router } from '@angular/router';
 })
 export class AddPropertyComponent implements OnInit {
 @ViewChild('Form') addPropertyForm: NgForm;
+@ViewChild('formTabs') formTabs?: TabsetComponent;
+//wil come from msters
+propertyTypes : Array<String> = ['House','Apartment','Duplex']
+furnishTypes : Array<String> = ['Fully','Semi','Unfurnished']
+propertyMoveDirections : Array<String> = ['East','West','South','North']
+
+propertyView = {};
   constructor(private router:Router) { }
 
   ngOnInit() {
@@ -20,4 +28,9 @@ export class AddPropertyComponent implements OnInit {
     console.log('Contracts, form submited');
     console.log(this.addPropertyForm);
   }
+  selectTab(tabId: number) {
+    if (this.formTabs?.tabs[tabId]) {
+      this.formTabs.tabs[tabId].active = true;
+    }
+  }  
 }
